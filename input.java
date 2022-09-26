e924dcf42c147cb1ee551cd89a9cf2092c29c74c
Bishnupriya Sethi <111557388+Bishnupri@users.noreply.github.com>
2022-09-26 13:34:55 +0530

    Update and rename sep26-4.java to input.java

--- input.java
+++ input.java
@@ -1,6 +1,6 @@
-
+// Get input from user
 import java.util.Scanner;
-class GetInputData{
+class Input{
  public static void main(String args[]){
  Scanner s= new Scanner(System.in);
 System.out.println("Put a number");
