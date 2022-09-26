6943905843c3cb103e366ebebd451e659f3cbe0d
Bishnupriya Sethi <111557388+Bishnupri@users.noreply.github.com>
2022-09-26 12:55:43 +0530

    Add files via upload

---
+++ amstrong.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class amstrong {
+public static void main(String[] args)   {
+int n, count = 0, a, b, c, sum = 0;
+try (Scanner sc = new Scanner(System.in)) {
+System.out.print("Enter  integer  to check:
+n = sc.nextInt();
+}
+a = n;
+ c = n;
+ while(a > 0)
+ {
+a = a / 10;	           
+count++;	           
+ }	        
+while(n > 0)	       
+ {	       
+b = n % 10;	           
+sum = (int) (sum+Math.pow(b, count));	      
+n = n / 10;
+}	        
+if(sum == c)	        
+{	        
+System.out.println("Number is Armstrong");	           
+  }	        
+ else
+{
+System.out.println("Number is not Armstrong");
+}
+}
+}
+	      
