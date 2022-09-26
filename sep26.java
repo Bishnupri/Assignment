3345076b165a9f7ed5a27b1f5926777e8fd8a08a
Bishnupriya Sethi <111557388+Bishnupri@users.noreply.github.com>
2022-09-26 12:30:44 +0530

    Add files via upload

---
+++ sep26.java
@@ -0,0 +1,10 @@
+public class MultiplyTwoNumbers{
+  public static void main(String args[]){
+  float a,b,c; 
+a = 5.5f;
+ b= 8.4f;
+ c  = a*b;
+System.out.println(" multiplication of two number is:" +c);
+}
+}
+
